edbe1ce92f82e3516892652a58c9c4c879e7dd6d
Husameldeen <mhusam87@gmail.com>
2026-06-04 09:26:22 +0200

    adding wild oasis website

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "vwjnjvwretfvnjuiqska.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+  },
   reactCompiler: true,
 };
 
